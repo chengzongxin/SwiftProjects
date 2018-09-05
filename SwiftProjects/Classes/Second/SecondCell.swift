46cb77d789e6da0b894bf8f1ba35bbfb41081430
Jason <8876142@qq.com>
2018-09-05 17:33:26 +0800

    CollectionView reorder

---
+++ SwiftProjects/Classes/Second/SecondCell.swift
@@ -0,0 +1,20 @@
+//
+//  SecondCell.swift
+//  SwiftProjects
+//
+//  Created by Jason on 2018/9/5.
+//  Copyright © 2018年 Jason. All rights reserved.
+//
+
+import UIKit
+
+class SecondCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- SwiftProjects/Classes/Second/SecondViewController.swift
+++ SwiftProjects/Classes/Second/SecondViewController.swift
@@ -8,29 +8,107 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+let secondCellID = "seconCellID"
+
+class SecondViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    lazy var dataSouces: [String] = {
+        var dataSouces: [String] = []
+        
+        for i in 0..<100 {
+            dataSouces.append("\(i)")
+        }
+        
+        return dataSouces
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        // Layout
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.scrollDirection = UICollectionViewScrollDirection.vertical // 设置水平显示
+        
+        layout.itemSize = CGSize(width: (view.frame.size.width - 40) / 3, height: (view.frame.size.width - 40) / 3)
+        
+        layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        
+        layout.minimumLineSpacing = 10.0
+        
+        layout.minimumInteritemSpacing = 10.0
+        
+        // CollectionView
+        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        collectionView.backgroundColor = UIColor.randomFlat
+        
+        collectionView.register(UINib.init(nibName: "SecondCell", bundle: nil), forCellWithReuseIdentifier: secondCellID)
+        
+        collectionView.delegate = self
+        
+        collectionView.dataSource = self
+        
+        // Gesture
+        let longPressGes = UILongPressGestureRecognizer(target: self, action: #selector(longPressAction(_:)))
+        
+        collectionView.addGestureRecognizer(longPressGes)
+        
+        return collectionView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor(gradientStyle: .topToBottom, withFrame: view.bounds, andColors: [UIColor.randomFlat,UIColor.randomFlat])
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        view.addSubview(collectionView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc fileprivate func longPressAction(_ gesture: UILongPressGestureRecognizer) {
+        // 判断长按的状态
+        switch gesture.state {
+        case .began:
+            // 获取选择位置
+            // indexPathForItem获取Item所处的indexPath
+            // indexPath是表格或列表推算出当前row和col的属性
+            // ***GestureRecognizer的location方法返回在所处view的一个CGPoint属性
+            guard let selectedIndexPath = self.collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else { return }
+            collectionView.beginInteractiveMovementForItem(at: selectedIndexPath)
+        case .changed:
+            // 移动了
+            collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: collectionView))
+        case .ended:
+            // 结束移动
+            collectionView.endInteractiveMovement()
+        default:
+            collectionView.cancelInteractiveMovement()
+        }
     }
-    */
-
 }
+
+extension SecondViewController {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSouces.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: secondCellID, for: indexPath) as! SecondCell
+        
+        cell.backgroundColor = UIColor.randomFlat
+        
+        cell.titleLabel.text = dataSouces[indexPath.item]
+        
+        return cell;
+    }
+    
+    // 实现重排的关键
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let tmpCell = dataSouces.remove(at: sourceIndexPath.item)
+        dataSouces.insert(tmpCell, at: destinationIndexPath.item)
+    }
+}
+
+
+
+
